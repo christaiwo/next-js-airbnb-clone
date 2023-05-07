a0417b98086001a1206d47d4872d1396a2c5ec47
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-05-07 11:33:01 +0100

    Added useRegisterModal

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
       <ClientOnly>
+        <Modal actionLabel='Submit' secondaryActionLabel="reg" title='Hello word' isOpen />
         <Navbar/>
       </ClientOnly>
       {children}
