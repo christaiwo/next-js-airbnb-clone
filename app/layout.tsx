a3ac8d20a3879b5b4192b438055c10cd529ccd8c
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-05-07 19:03:26 +0100

    update nextAuth

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from './components/navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import ToasterProvider from './providers/ToasterProvider'
+import LoginModal from './components/modals/LoginModal'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <ClientOnly>
         <ToasterProvider/>
         <RegisterModal />
+        <LoginModal />
         <Navbar/>
       </ClientOnly>
       {children}
