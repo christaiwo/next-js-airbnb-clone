bb1eba61b14fa3e70ba8536a90444d0e3b6f7574
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-05-07 12:34:11 +0100

    Added Input component

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -20,7 +20,6 @@ const RegisterModal = () => {
         handleSubmit,
         formState: {
             errors,
-
         }
     } = useForm<FieldValues>({
         defaultValues: {
@@ -46,6 +45,10 @@ const RegisterModal = () => {
     const bodyContent = (
         <div className="flex flex-col gap-4">
             <Heading title="Welcome to Airbnb" subtitle="Create an account"  />
+
+            <Input id="email" label="Email" disabled={isLoading} register={register} errors={errors} required />
+            <Input id="name" label="Name" disabled={isLoading} register={register} errors={errors} required />
+            <Input id="password" label="Password" type="password" disabled={isLoading} register={register} errors={errors} required />
         </div>
     );
 
