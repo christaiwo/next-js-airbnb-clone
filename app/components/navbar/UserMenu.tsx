8a8f3d49e18a24a6b489aab71f04dcdb16768cf5
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-05-07 10:16:38 +0100

    Update usermenu component added a div and a state to manage user menu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,7 +2,15 @@
 
 import {AiOutlineMenu} from 'react-icons/ai'
 import Avatar from '../Avatar';
+import { useCallback, useState } from 'react';
 const UserMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value) => !value);
+    }, []);
+
+
     return ( 
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
@@ -10,13 +18,21 @@ const UserMenu = () => {
                     Airbnb You home
                 </div>
 
-                <div className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-300 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
+                <div onClick={toggleOpen} className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-300 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
                     <AiOutlineMenu />
                     <div className="hidden md:block">
                         <Avatar />
                     </div>
                 </div>
             </div>
+
+            {isOpen && (
+                <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+                    <div className="flex flex-col cursor-pointer">
+                        
+                    </div>
+                </div>
+            )}
         </div>
      );
 }
