3093d5fb5e5536a006f0399fcd8a489e4ce00b85
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-05-08 07:57:24 +0100

    update usermenu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -23,11 +23,17 @@ const UserMenu: React.FC<UserMenuProps> = ({currentUser}) => {
     }, []);
 
 
+    const onRent = useCallback(() => {
+        if(!currentUser) return loginModal.onOpen();
+
+        // open rent modal
+    }, [currentUser, loginModal]);
+
     return ( 
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
-                <div className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
-                    Airbnb You home
+                <div onClick={onRent} className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
+                    Airbnb Your home
                 </div>
 
                 <div onClick={toggleOpen} className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-300 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
