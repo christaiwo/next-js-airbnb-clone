e5e3a914e7e1f694a7112761cef2b9aabc9be540
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-05-10 09:42:22 +0100

    feat: addest EmptyState component

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -23,11 +23,13 @@ const EmptyState: React.FC<EmptyStateProps> = ({
         <div className="h-[60vh] flex flex-col gap-2 justify-center items-center">
             <Heading center title={title} subtitle={subtitle} />
             <div className="w-48 mt-4">
-                <Button  
-                    outline
-                    label="Remove all filters"
-                    onClick={() => router.push('/')}
-                />
+                {showReset && (
+                    <Button  
+                        outline
+                        label="Remove all filters"
+                        onClick={() => router.push('/')}
+                    />
+                )}
             </div>
         </div>
     );
