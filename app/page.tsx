5b6d3ce3c1afeff2fbcdd7456895c6d844c0bf27
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-05-11 05:56:29 +0100

    feat: Listing card component

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import ClientOnly from './components/ClientOnly'
 import Container from './components/Container'
 import EmptyState from './components/EmptyState';
 import getListing from './actions/getListings';
+import ListingCard from './components/listings/ListingCard';
+import { getCurrentUser } from './actions/getCurrentUser';
 
 export default async function Home() {
   const listings = await getListing();
-  const isEmpty = true;
+  const currentUser = await getCurrentUser();
 
   if(listings.length === 0){
     return (
@@ -20,9 +22,9 @@ export default async function Home() {
       <ClientOnly>
         <Container>
           <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-              <div>
-                My feature listing
-              </div>
+              {listings.map((listing, id) => (
+                <ListingCard key={id} data={listing} />
+              ))}
           </div>
         </Container>
       </ClientOnly>
