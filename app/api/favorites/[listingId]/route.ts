c28b308bf75cc005fe30ef247a891c06cefd3394
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-05-11 06:53:42 +0100

    feat: added DELETE request to remove favorites id

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -34,3 +34,27 @@ export async function POST(request: Request, {params}: {params: IParams}) {
 
 
 
+export async function DELETE(request: Request, {params}: {params: IParams}) {
+    const currentUser = await getCurrentUser();
+
+    if(!currentUser) return NextResponse.error();
+
+    const { listingId } = params;
+
+    if(!listingId || typeof listingId !== 'string') throw new Error('Invalid ID');
+
+    let favoriteIds = [...(currentUser.favoriteIds) || []];
+    
+    favoriteIds = favoriteIds.filter((id) => id !== listingId);
+
+    const user = await prisma.user.update({
+        where: {
+            id: currentUser.id
+        },
+        data: {
+            favoriteIds
+        }
+    })
+
+    return NextResponse.json(user);
+}
