1f57f0aec2519ab225b836c1256bbb08d5575eca
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-05-12 06:37:35 +0100

    feat: added ListingReservation component

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -3,6 +3,7 @@
 import Container from "@/app/components/Container"
 import ListingHead from "@/app/components/listings/ListingHead"
 import ListingInfo from "@/app/components/listings/ListingInfo"
+import ListingReservation from "@/app/components/listings/ListingReservation"
 import { categories } from "@/app/components/navbar/Categories"
 import useLoginModal from "@/app/hooks/useLoginModal"
 import { SafeListing, SafeUser } from "@/app/types"
@@ -121,6 +122,18 @@ const ListingClient: React.FC<ListingClientProps> = ({reservations = [], listing
                             bathroomCount={listing.bathroomCount}
                             locationValue={listing.locationValue}
                         />
+
+                        <div className="order-first mb-10 md:order-last md:col-span-3">
+                            <ListingReservation 
+                                price={listing.price}
+                                totalPrice={totalPrice}
+                                onChangeDate={(value) => setDateRange(value)}
+                                dateRange={dateRange}
+                                onSubmit={onCreateReservation}
+                                disabled={isLoading}
+                                disabledDates={disabledDate}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
