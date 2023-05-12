03de1196a478fe7b18291da5f062546d71ef7d0d
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-05-12 12:07:53 +0100

    feat: Created FavoritesClient component

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,12 +1,27 @@
+import { getCurrentUser } from "../actions/getCurrentUser";
+import getFavoriteListings from "../actions/getFavoriteListing";
 import ClientOnly from "../components/ClientOnly";
 import EmptyState from "../components/EmptyState";
+import FavoritesClient from "./FavoritesClient";
 
 const FavoritesPage = async() => {
+    const listings = await getFavoriteListings();
+    const currentUser = await getCurrentUser();
 
+    if(listings.length === 0) {
+        return (
+            <ClientOnly>
+                <EmptyState title="No favorites" subtitle="Looks like you have no favorite listings" />
+            </ClientOnly>
+        )
+    }
 
     return (  
         <ClientOnly>
-            <EmptyState title="No favorites" subtitle="Looks like you have no favorite listings" />
+            <FavoritesClient 
+                listings={listings}
+                currentUser={currentUser}
+            />
             
         </ClientOnly>
     );
